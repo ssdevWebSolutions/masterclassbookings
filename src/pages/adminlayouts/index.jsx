98c84e5a66c2198a005b69e2c8e02718fed4ea3a
ssdevWebSolutions <ssdevwebsolutions@gmail.com>
2026-01-10 20:04:55 +0530

    "7"

--- src/pages/adminlayouts/index.jsx
+++ src/pages/adminlayouts/index.jsx
@@ -4,10 +4,11 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 
 import { Box } from "@mui/material";
-import Sidebar from "../../pages/admin/components/Sidebar";
-import MainHeader from "../admin/components/MainHeader";
-import DashboardLayout from "../admin/components/DashboardLayout";
-import AuthGuard from "../admin/components/AuthGuard";
+import Sidebar from '../admin/components/Sidebar';
+import MainHeader from '../admin/components/MainHeader';
+import DashboardLayout from '../admin/components/DashboardLayout';
+import AuthGuard from '../admin/components/AuthGuard';
+
 
 
 export default function AdminLayout({ children, activeNav }) {
